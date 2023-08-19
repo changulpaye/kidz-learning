bc602dd8d7afa3cf250496c66a625b74f843cb17
Param <changulpaye@gmail.com>
2023-08-19 15:49:07 +0530

    multiplication table

---
+++ src/components/Numbertable.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import classes from "./NumberTable.module.css";
+
+const NumberTable = () => {
+  const [enteredNum, setEnteredNum] = useState("");
+  const [table, setTable] = useState([]);
+  const handleInputChange = (event) => {
+    setEnteredNum(event.target.value);
+  };
+  return (
+    <div>
+      <div className={classes.list}>
+       
+        <input onChange={handleInputChange} value={enteredNum} type="number" placeholder="Enter Number" />
+      </div>
+      <ul className={classes.listStyle}>
+        {/* { table.map((row, i) => (
+          <li key={i}> {row} </li>
+        ))} */}
+        {enteredNum &&
+          Array(10)
+            .fill({})
+            .map((d, i) => {
+              let multiplier = i + 1;
+              return (
+                <li>
+                  <h1>{enteredNum}</h1> x <h1> {multiplier} </h1> ={" "}
+                  <h1> {enteredNum * multiplier} </h1>
+                </li>
+              );
+            })}
+      </ul>
+    </div>
+  );
+};
+
+export default NumberTable;
---
+++ src/components/header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header>Kidz Learning</header>;
+};
+
+export default Header;
